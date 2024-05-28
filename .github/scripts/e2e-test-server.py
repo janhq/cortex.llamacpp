8fc83d46ba923a0f2926335e24f0abc4f3c5453d
vansangpfiev <sang@jan.ai>
2024-05-28 16:17:31 +0700

    fix: subprocess

--- .github/scripts/e2e-test-server.py
+++ .github/scripts/e2e-test-server.py
@@ -51,7 +51,7 @@ port = random.randint(10000, 11000)
 
 cwd = os.getcwd()
 print(cwd)
-p = subprocess.Popen(cwd + '/' + BINARY_PATH + ' 127.0.0.1 ' + str(port))
+p = subprocess.Popen([cwd + '/' + BINARY_PATH, '127.0.0.1', str(port)])
 print("Server started!")
 
 logging.basicConfig(filename='./test.log',
