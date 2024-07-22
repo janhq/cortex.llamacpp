0e2b019c6e3386a81f63f84dd7cf52f08da587ea
vansangpfiev <sang@jan.ai>
2024-07-22 16:37:57 +0700

    fix: set embedding parameter default to true if use embedding only model

--- src/llama_engine.cc
+++ src/llama_engine.cc
@@ -335,8 +335,10 @@ bool LlamaEngine::LoadModelImpl(std::shared_ptr<Json::Value> json_body) {
 
     params.n_gpu_layers = json_body->get("ngl", 100).asInt();
     params.n_ctx = json_body->get("ctx_len", 2048).asInt();
-    params.embedding = json_body->get("embedding", false).asBool();
     model_type = json_body->get("model_type", "llm").asString();
+    // In case of embedding only model, we set default = true
+    params.embedding =
+        json_body->get("embedding", model_type == "embedding").asBool();
     params.n_batch = json_body->get("n_batch", 2048).asInt();
     params.n_ubatch = json_body->get("n_ubatch", params.n_batch).asInt();
     // Check if n_parallel exists in json_body, if not, set to drogon_thread
